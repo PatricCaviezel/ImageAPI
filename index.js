b218d38fdb032d8f92326adf925f1bd08d6418a4
Patric Caviezel <patric.caviezel@gmail.com>
2023-05-03 11:36:56 +0200

    expanded api and added routes

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const port = 3000;
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Welcome')
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/bar', (req, res) => {
+    res.sendFile(__dirname + '/public/bar.html');
+});
+
+app.get('/pie', (req, res) => {
+    res.sendFile(__dirname + '/public/pie.html');
+});
+
+app.get('/line', (req, res) => {
+    res.sendFile(__dirname + '/public/line.html');
 });
 
 app.get('/chartdata', (req, res) => {
 
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
