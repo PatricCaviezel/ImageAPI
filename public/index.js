b0e5c17f147dda859a331bca2106c16d1835125f
PatricCaviezel <patric.caviezel@gmail.com>
2023-05-15 13:41:11 +0200

    diagram gets displayed in web as buffer

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ let select = () => {
     window.location = window.location + selected;
 }
 
-let check = () => {
+let check = async () => {
     let data = {}
     let values = {};
     data.type = (window.location.toString()).split("/").pop()+"chart";
@@ -30,11 +30,14 @@ let check = () => {
         }
     }
     data.values = values;
-    fetch('http://localhost:3000/image', {
+    let returnData = await fetch('http://localhost:3000/image', {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
             'Content-Type': "application/json"
         }
     });
+    returnData = await returnData.json()
+    let diagram = document.getElementById('diagram');
+    diagram.src = returnData.src;
 }
