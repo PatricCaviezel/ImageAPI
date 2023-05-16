9ca80aa487a1753df2c436526827dd7dba588c2f
PatricCaviezel <patric.caviezel@gmail.com>
2023-05-16 10:13:50 +0200

    fixed json path

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 require 'json'
 
-json_data = File.read('test.json')
+json_data = File.read('buffers.json')
 data = JSON.parse(json_data)
 
 data['buffers'] ||= []
 data['buffers'] << JSON.parse(ARGV[0])
 
-File.open('test.json', 'w') do |file|
+File.open('buffers.json', 'w') do |file|
   file.write(JSON.pretty_generate(data))
 end
